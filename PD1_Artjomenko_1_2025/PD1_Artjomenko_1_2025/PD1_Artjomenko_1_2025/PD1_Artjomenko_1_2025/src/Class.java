d4e064e791b3163d2e47e638d7d8cd99f97d5ea7
julianna964 <juliannaart1260@gmail.com>
2025-06-04 12:53:12 +0300

    Add files via upload

---
+++ PD1_Artjomenko_1_2025/PD1_Artjomenko_1_2025/PD1_Artjomenko_1_2025/PD1_Artjomenko_1_2025/src/Class.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+class User{
+ private String name;
+ private String login;
+ private String password;
+
+ private void User(String name, String login, String password){
+
+ }
+
+ private boolean enter(String login, String password){
+ return true;
+ }
+ }
+
+class Student{
+    private int questionsCount = 0;
+    private int rightAnswers = 0;
+    
+     private void getAnswer(String question, String answer){
+
+ }
+    
+}
+
+class Question{
+    private String text;
+    private String answer;
+    
+  private void Question (String text, String answer){
+
+ }  
+  private void getText (){
+
+ }  
+  
+ }
+   
+class DistanceExamenator{
+    private String users;
+    private String questions;
+    private String currentUser;
+    
+    private void addUser(String name, String login, String password, String repeation){
+        
+    }
+    
+    private void findUser(String login, String password){
+        
+    }
+    
+    private void save(){
+        
+    }
+    
+    private void load(){
+        
+    }
+     private void getResults(){
+         
+     }
+}
+
